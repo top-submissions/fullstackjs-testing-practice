577774dbb93593bc0e52c335529cc52088bd3f3a
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-08 20:46:54 +0800

    test(capitalize): add if first letter is capitalized

---
+++ src/test/script.test.js
@@ -0,0 +1,9 @@
+import { capitalize } from './utils.js';
+
+describe('utils module', () => {
+  describe('capitalize function', () => {
+    test('first letter of "odin" is capitalized (Odin)', () => {
+      expect(capitalize('odin')).toBe('Odin');
+    });
+  });
+});
