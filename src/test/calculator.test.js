270516e143f9645bfbbaa76769d21f19df293b47
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-09 15:22:31 +0800

    refactor: make own test file for calculator

---
+++ src/test/calculator.test.js
@@ -0,0 +1,11 @@
+import { calculator } from '../calculator.js';
+
+describe('calculator module', () => {
+  describe('calculator object', () => {
+    describe('add function', () => {
+      test('adds numbers 1 + 2 => 3', () => {
+        expect(calculator.add(1, 2)).toBe(3);
+      });
+    });
+  });
+});
--- src/test/utils.test.js
+++ src/test/utils.test.js
@@ -1,5 +1,4 @@
 import { capitalize, reverseString } from '../utils.js';
-import { calculator } from '../calculator.js';
 
 describe('utils module', () => {
   describe('capitalize function', () => {
@@ -14,16 +13,6 @@ describe('utils module', () => {
   describe('reverseString function', () => {
     test('reverses string "odin" => "nido"', () => {
       expect(reverseString('odin')).toBe('nido');
-    });
-  });
-});
-
-describe('calculator module', () => {
-  describe('calculator object', () => {
-    describe('add function', () => {
-      test('adds numbers 1 + 2 => 3', () => {
-        expect(calculator.add(1, 2)).toBe(3);
-      });
     });
   });
 });
