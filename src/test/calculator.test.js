465ca9114a71c336e8c31d35886f16e398cbef62
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-09 15:26:16 +0800

    test(add): can calculate multiple number args

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -4,7 +4,7 @@ describe('calculator module', () => {
   describe('calculator object', () => {
     describe('add function', () => {
       test('adds numbers 1 + 2 => 3', () => {
-        expect(calculator.add(1, 2)).toBe(3);
+        expect(calculator.add(1, 2, 3)).toBe(6);
       });
     });
   });
