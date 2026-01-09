358a63b68046b8b68c939db1f16c0c891efb4876
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-09 21:15:34 +0800

    feat(calculator): add subtract function

--- src/calculator.js
+++ src/calculator.js
@@ -2,6 +2,9 @@ let calculator = {
   add: function (...numbers) {
     return numbers.reduce((sum, number) => sum + number, 0);
   },
+  subtract: function (...numbers) {
+    return numbers.reduce((diff, number) => diff - number);
+  },
 };
 
 export { calculator };
